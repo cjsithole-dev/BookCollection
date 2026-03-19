d239551149c2176e2abee5a930e6b1b3f7b9d879
sitho <sitho@DESKTOP-5NQPA6P>
2026-03-19 11:28:16 +0200

    Add project files.

---
+++ BookCollection/CfrmBookCollection.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace BookCollection
+{
+    partial class CfrmBookCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbGenre = new System.Windows.Forms.ComboBox();
+            this.btnViewCollection = new System.Windows.Forms.Button();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.lstbxBooks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ISBN:";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(55, 11);
+            this.txtISBN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(137, 20);
+            this.txtISBN.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(55, 41);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(179, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TItle:";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(55, 71);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(179, 20);
+            this.txtAuthor.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Author:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 110);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Genre:";
+            // 
+            // cmbGenre
+            // 
+            this.cmbGenre.FormattingEnabled = true;
+            this.cmbGenre.Location = new System.Drawing.Point(55, 101);
+            this.cmbGenre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbGenre.Name = "cmbGenre";
+            this.cmbGenre.Size = new System.Drawing.Size(123, 21);
+            this.cmbGenre.TabIndex = 7;
+            // 
+            // btnViewCollection
+            // 
+            this.btnViewCollection.Location = new System.Drawing.Point(262, 46);
+            this.btnViewCollection.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnViewCollection.Name = "btnViewCollection";
+            this.btnViewCollection.Size = new System.Drawing.Size(95, 28);
+            this.btnViewCollection.TabIndex = 8;
+            this.btnViewCollection.Text = "View Collection";
+            this.btnViewCollection.UseVisualStyleBackColor = true;
+            this.btnViewCollection.Click += new System.EventHandler(this.btnViewCollection_Click);
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(262, 12);
+            this.btnAddBook.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(95, 28);
+            this.btnAddBook.TabIndex = 9;
+            this.btnAddBook.Text = "Add Book";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // lstbxBooks
+            // 
+            this.lstbxBooks.FormattingEnabled = true;
+            this.lstbxBooks.HorizontalScrollbar = true;
+            this.lstbxBooks.Location = new System.Drawing.Point(12, 141);
+            this.lstbxBooks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstbxBooks.Name = "lstbxBooks";
+            this.lstbxBooks.Size = new System.Drawing.Size(345, 121);
+            this.lstbxBooks.TabIndex = 11;
+            // 
+            // CfrmBookCollection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 275);
+            this.Controls.Add(this.lstbxBooks);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.btnViewCollection);
+            this.Controls.Add(this.cmbGenre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "CfrmBookCollection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Collection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbGenre;
+        private System.Windows.Forms.Button btnViewCollection;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.ListBox lstbxBooks;
+    }
+}
+
---
+++ BookCollection/CfrmBookCollection.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookCollection
+{
+
+    // Added enum and Book model used by the form
+    public enum Genre
+    {
+        HistoricalFiction,
+        NonFiction,
+        Mystery,
+        Romance,
+        Fantasy,
+        Thriller,
+        Other
+    }
+
+    public class Book
+    {
+        public string ISBN { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public Genre Genre { get; }
+
+        public Book(string isbn, string title, string author, Genre genre)
+        {
+            ISBN = isbn ?? string.Empty;
+            Title = title ?? string.Empty;
+            Author = author ?? string.Empty;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{ISBN} - {Title} by {Author} ({Genre})";
+        }
+    }
+    public partial class CfrmBookCollection : Form
+    {
+        private readonly List<Book> m_books = new List<Book>();
+
+        public CfrmBookCollection()
+        {
+            InitializeComponent();
+            cmbGenre.Items.Add("HistoricalFiction");
+            cmbGenre.Items.Add("NonFiction");
+            cmbGenre.Items.Add("Mystery");
+            cmbGenre.Items.Add("Romance");
+            cmbGenre.Items.Add("Fantasy");
+            cmbGenre.Items.Add("Thriller");
+            cmbGenre.Items.Add("Other");
+
+            AddTestData();
+        }
+
+
+        private void AddTestData()
+        {
+            m_books.Add(new Book("9780143127741", "The Nightingale", "Kristin Hannah", Genre.HistoricalFiction));
+
+            
+            m_books.Add(new Book("9780062315007", "Sapiens: A Brief History of Humankind", "Yuval Noah Harari", Genre.NonFiction));
+
+            
+            m_books.Add(new Book("9780307277671", "The Girl with the Dragon Tattoo", "Stieg Larsson", Genre.Mystery));
+        }
+
+        private void btnViewCollection_Click(object sender, EventArgs e)
+        {
+            lstbxBooks.Items.Clear();
+
+            foreach (Book b in m_books)
+            {
+                lstbxBooks.Items.Add(b.ToString());
+            }
+
+        }//btnViewCollection_Click
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+
+            string isbn = txtISBN.Text.Trim();
+            string title = txtTitle.Text.Trim();
+            string author = txtAuthor.Text.Trim();
+            string genreText = cmbGenre.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                MessageBox.Show("Please enter a title.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (Enum.TryParse<Genre>(genreText, true, out Genre parsedGenre))
+            {
+                var book = new Book(isbn, title, author, parsedGenre);
+                m_books.Add(book);
+
+                MessageBox.Show("Book added to collection.", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Optionally clear fields after successful add
+                txtISBN.Clear();
+                txtTitle.Clear();
+                txtAuthor.Clear();
+                cmbGenre.SelectedIndex = -1;
+            }
+            else
+            {
+                MessageBox.Show("Invalid genre specified.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+        
+    }//class
+}//namespace
